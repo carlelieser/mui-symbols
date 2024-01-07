ffed1bb45c9b5aaf0d66987019300fb1c35b0fd6
Carlos <carlos@prolydian.com>
2024-01-07 07:33:10 -0500

    create svgo plugin for removing clip-paths

---
+++ scripts/plugins/remove-clip-path.mjs
@@ -0,0 +1,17 @@
+export const removeClipPath = {
+	name: "removeClipPath",
+	description: "Remove all clip-path attributes and elements.",
+	fn: () => {
+		return {
+			element: {
+				enter: (node, parentNode) => {
+					const isClipPath = node.name === "clip-path" || node.name === "clipPath";
+					const clipPathAttribute = node.attributes["clipPath"];
+					if (!isClipPath && !clipPathAttribute) return;
+					if (isClipPath) parentNode.children = parentNode.children.filter(child => child !== node);
+					if (clipPathAttribute) delete node.attributes["clipPath"];
+				},
+			},
+		};
+	},
+};
