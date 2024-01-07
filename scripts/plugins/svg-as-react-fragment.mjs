021038c5f6173b1736ebb3798794354c9d41f301
Carlos <carlos@prolydian.com>
2024-01-07 07:31:48 -0500

    make svgAsReactFragment a plugin

---
+++ scripts/plugins/svg-as-react-fragment.mjs
@@ -0,0 +1,40 @@
+export const svgAsReactFragment = ({
+									   onChildrenAsArray = () => {
+									   },
+								   }) => ({
+	name: "svgAsReactFragment",
+	fn: () => {
+		return {
+			root: {
+				enter(root) {
+					const [svg, ...rootChildren] = root.children;
+
+					if (rootChildren.length > 0) {
+						throw new Error(
+							"Expected a single child of the root",
+						);
+					}
+
+					if (
+						svg.type !== "element" ||
+						svg.name !== "svg"
+					) {
+						throw new Error(
+							"Expected an svg element as the root child",
+						);
+					}
+
+					if (svg.children.length > 1) {
+						onChildrenAsArray();
+						svg.children.forEach((svgChild, index) => {
+							svgChild.attributes.key = index;
+							svgChild.name = `SVGChild:${svgChild.name}`;
+						});
+					}
+
+					root.children = svg.children;
+				},
+			},
+		};
+	},
+});
